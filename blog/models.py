433c165d639547d6511ecb6ee16962ba8baf9e58
justphantom <justphantom@outlook.com>
2020-06-29 00:04:57 +0800

    增加后台管理Post,Category,Tag

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,23 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -20,3 +34,11 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+    def __str__(self):
+        return self.title
