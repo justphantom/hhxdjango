ff80c9766dca471bbcd3058130dfd47ebcb3d959
justphantom <justphantom@outlook.com>
2020-06-28 22:36:21 +0800

    allow all host

--- hhxdjango/settings.py
+++ hhxdjango/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '%j_6ej^=s!fo-olir+i5az_^wr=1#ci1s5u%e6a9s!m1cr_i=k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
