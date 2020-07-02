9db5a0d4adec8bdb47689e9f7526a9257b23c38a
justphantom <justphantom@outlook.com>
2020-07-02 16:06:19 +0800

    调整样式

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:pk>', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 import markdown
+from django.contrib import messages
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
@@ -22,3 +24,12 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def search(request):
+    q = request.POST.get('q')
+    if not q:
+        error_msg = "请输入关键词"
+        messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'post_list': post_list})
