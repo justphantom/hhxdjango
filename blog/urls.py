51715341b467bbfc31a66ac36bca1cb6a30f6170
justphantom <justphantom@outlook.com>
2020-07-07 21:35:25 +0800

    取消类视图

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('post/<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>', views.detail, name='detail'),
     path('search/', views.search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ import markdown
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-
+# from django.views import generic
 from .models import Post
 
 
@@ -10,20 +10,45 @@ from .models import Post
 
 
 # Create your views here.
+# class IndexView(generic.ListView):
+#     model = Post
+#     template_name = 'blog/index.html'
+
+
+# class DetailView(generic.DetailView):
+#     model = Post
+#     template_name = 'blog/detail.html'
+#     context_object_name = 'post'
+#
+#     def get_object(self, queryset=None):
+#         post = super().get_object(queryset=None)
+#         md = markdown.Markdown(
+#             extensions=['markdown.extensions.extra', 'markdown.extensions.codehilite', 'markdown.extensions.toc'])
+#         post.body = md.convert(post.body)
+#         return post
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = context.get(post.title, None)
+#         print(context['title'])
+#         return context
+
+
 def index(request):
     post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    return render(request, 'blog/index.html', context={'post_list': post_list, 'title': 'Blog'})
 
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_view()
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    return render(request, 'blog/detail.html', context={'post': post, 'title': post.title})
 
 
 def search(request):
