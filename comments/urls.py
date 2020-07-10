77495bd1d0e5045a4c80534d6e73982af0cdd4af
justphantom <justphantom@outlook.com>
2020-07-10 09:05:57 +0800

    fix url config

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'comments'
 urlpatterns = [
-    path('comment/<int:post_pk>', views.comment, name='comment'),
+    path('<int:post_pk>', views.comment, name='comment'),
 ]
--- hhxdjango/urls.py
+++ hhxdjango/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('comments.urls')),
-    path('', include('blog.urls')),
+    path('comment/', include('comments.urls')),
+    path('', views.index),
     path('book/', include('books.urls')),
 ]
