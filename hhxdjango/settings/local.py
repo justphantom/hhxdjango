af9ac7c1119087f101ff7ef516653ebc05b03c3b
justphantom <justphantom@outlook.com>
2020-07-14 09:25:07 +0800

    修改数据连接

--- hhxdjango/settings/local.py
+++ hhxdjango/settings/local.py
@@ -6,3 +6,17 @@ SECRET_KEY = '%j_6ej^=s!fo-olir+i5az_^wr=1#ci1s5u%e6a9s!m1cr_i=k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hhxdjango',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
--- hhxdjango/settings/production.py
+++ hhxdjango/settings/production.py
@@ -1,6 +1,19 @@
 from .common import *
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '%j_6ej^=s!fo-olir+i5az_^wr=1#ci1s5u%e6a9s!m1cr_i=k'
+# SECRET_KEY = '%j_6ej^=s!fo-olir+i5az_^wr=1#ci1s5u%e6a9s!m1cr_i=k'
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DJANGO_DB_NAME'],
+        'USER': os.environ['DJANGO_DB_USER'],
+        'PASSWORD': os.environ['DJANGO_DB_PASS'],
+        'HOST': os.environ['DJANGO_DB_HOST'],
+        'PORT': os.environ['DJANGO_DB_PORT'],
+    }
+}
