31f1c5f85668f9e3cc9b42bd933c4bdb60d915d8
justphantom <justphantom@outlook.com>
2020-07-14 21:44:04 +0800

    增加父级字段

--- books/models.py
+++ books/models.py
@@ -19,7 +19,9 @@ class Chapter(models.Model):
     title = models.CharField('标题', max_length=200)
     index = models.IntegerField('顺序')
     body = models.TextField('正文')
-    book = models.ForeignKey(BookInfo, verbose_name='书籍', on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        BookInfo, verbose_name='书籍', on_delete=models.CASCADE)
+    parent = models.IntegerField('上级章节', default=0)
 
     class Meta:
         verbose_name = '章节'
