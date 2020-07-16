c9c19ef140fdbde4a0d78f8c7e7e0c4bdc310c5d
justphantom <54788905+justphantom@users.noreply.github.com>
2020-07-16 12:14:42 +0800

    Update common.py
    增加rest frame

--- hhxdjango/settings/common.py
+++ hhxdjango/settings/common.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'blog.apps.BlogConfig',
     'comments.apps.CommentsConfig',
     'books.apps.BooksConfig',
