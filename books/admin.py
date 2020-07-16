f9752e538bda0646a083cfe10d94d4687c848dc3
justphantom <justphantom@outlook.com>
2020-07-16 14:46:47 +0800

    注册author模型

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import BookInfo, Chapter
+from .models import BookInfo, Chapter, Author
+
 # Register your models here.
 admin.site.register(BookInfo)
 admin.site.register(Chapter)
+admin.site.register(Author)
