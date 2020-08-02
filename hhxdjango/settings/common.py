c1afc1e959b6b2b2a61e865daa0df3a26c388287
justphantom <54788905+justphantom@users.noreply.github.com>
2020-08-02 17:56:46 +0800

    Update common.py
    修改权限类型

--- hhxdjango/settings/common.py
+++ hhxdjango/settings/common.py
@@ -109,5 +109,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAdminUser", ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly", ],
 }
