de03556395df962206cef56d18e2e122d7e8a747
justphantom <justphantom@outlook.com>
2020-08-05 23:30:50 +0800

    增加字段

--- basic/models.py
+++ basic/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+
+
 class ProcessInfo(models.Model):
     processno = models.CharField('工序编号', max_length=20)
     processname = models.CharField('工序名称', max_length=100)
+    createtime = models.DateTimeField('创建时间', auto_now_add=True)
+    modifytime = models.DateTimeField('修改时间', auto_now=True)
 
     class Meta:
         verbose_name = '工序信息'
