977e63737d832087c0532556b1cf096b3a85768f
justphantom <justphantom@outlook.com>
2020-08-06 00:02:07 +0800

    展示全部字段

--- basic/admin.py
+++ basic/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import ProcessInfo
 
+
 # Register your models here.
-admin.site.register(ProcessInfo)
+class ProcessInfoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProcessInfo._meta.get_fields()]
+
+
+admin.site.register(ProcessInfo, ProcessInfoAdmin)
--- basic/apps.py
+++ basic/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class BasicConfig(AppConfig):
     name = 'basic'
+    verbose_name = '基础资料'
