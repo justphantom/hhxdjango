e1600c76c065b9fa7bcf868bb7959994ee4764cc
justphantom <justphantom@outlook.com>
2020-08-30 20:55:00 +0800

    增加一些基础资料类型

--- basic/admin.py
+++ basic/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import ProcessInfo
+from .models import ProcessInfo, ProductInfo, ProductType
 
 
 # Register your models here.
 class ProcessInfoAdmin(admin.ModelAdmin):
-    list_display = ['processno', 'processname']
+    list_display = ['code', 'name']
     # list_editable = ['processname']
-    search_fields = ["processname"]
+    search_fields = ["name"]
 
 
 admin.site.register(ProcessInfo, ProcessInfoAdmin)
+admin.site.register(ProductInfo)
+admin.site.register(ProductType)
--- basic/models.py
+++ basic/models.py
@@ -5,15 +5,47 @@ from django.db import models
 
 
 class ProcessInfo(models.Model):
-    processno = models.CharField('工序编号', max_length=20)
-    processname = models.CharField('工序名称', max_length=100)
+    code = models.CharField('工序编号', max_length=20)
+    name = models.CharField('工序名称', max_length=100)
     createtime = models.DateTimeField('创建时间', auto_now_add=True)
     modifytime = models.DateTimeField('修改时间', auto_now=True)
 
     class Meta:
         verbose_name = '工序信息'
         verbose_name_plural = verbose_name
-        ordering = ['processno']
+        ordering = ['code']
 
     def __str__(self):
-        return self.processname
+        return self.name
+
+
+class ProductType(models.Model):
+    code = models.CharField('类型编号', max_length=10)
+    name = models.CharField('类型名称', max_length=20)
+    createtime = models.DateTimeField('创建时间', auto_now_add=True)
+    modifytime = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        verbose_name = '产品类型'
+        verbose_name_plural = verbose_name
+        ordering = ['code']
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInfo(models.Model):
+    code = models.CharField('产品编号', max_length=20)
+    name = models.CharField('产品名称', max_length=50)
+    model = models.CharField('产品型号', max_length=50)
+    type = models.ForeignKey(ProductType, verbose_name='产品类型', on_delete=models.PROTECT)
+    createtime = models.DateTimeField('创建时间', auto_now_add=True)
+    modifytime = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        verbose_name = '产品信息'
+        verbose_name_plural = verbose_name
+        ordering = ['code']
+
+    def __str__(self):
+        return self.name
